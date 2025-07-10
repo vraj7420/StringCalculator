53cc6b4b3da7534606409a8106e7ce41c5bd758f
Vraj <vraj.brahmbhatt@coorb.io>
2025-07-10 18:54:16 +0530

    initial commit

---
+++ StringCalculator/StringCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorApp.swift
+//  StringCalculator
+//
+//  Created by Vraj on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct StringCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  StringCalculatorTests.swift
+//  StringCalculatorTests
+//
+//  Created by Vraj on 10/07/25.
+//
+
+import Testing
+
+struct StringCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
